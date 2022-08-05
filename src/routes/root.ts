3948e887e8282c0d488a2abb65954df1c38a27aa
gaeldumon <dmnleag@protonmail.com>
2022-08-05 22:21:13 +0200

    Add two controllers function which get stud id and externals ids based on email. Create send-document route which retrieve data needed to send base64 file.

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,21 +1,38 @@
 import {FastifyPluginAsync} from 'fastify'
 import got from "got";
+import {apiHeaders} from "../globals/headers";
+import {ISendDocumentBody} from "../interfaces/ISendDocumentBody";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+
+    async function getStudentIdByEmail(email: string): Promise<string> {
+        const url = `https://ext.edusign.fr/v1/student/by-email/${email}`
+        const {result} = await got(url, {headers: apiHeaders}).json()
+        return result.id
+    }
+
+    async function getExternalIdsByEmails(emails: string[]): Promise<string[]> {
+        let ids = []
+        for (const email of emails) {
+            const url = `https://ext.edusign.fr/v1/externals/by-email/${email}`
+            const {result} = await got(url, {headers: apiHeaders}).json()
+            ids.push(result.ID)
+        }
+        return ids
+    }
 
     fastify.get('/', async function (request, reply) {
         return "Welcome to the EduDocs API"
     })
 
-    fastify.get('/student-by-email/:email', async function (request, reply) {
+    fastify.post('/send-document', async function (request, reply) {
         // @ts-ignore
-        const studentEmail = request.params.email;
-        const edusignUrl = `https://ext.edusign.fr/v1/student/by-email/${studentEmail}`
-        const headers = {
-            'Authorization': `Bearer ${process.env.EDUSIGN_BEARER_TOKEN}`,
-            'Content-Type': 'application/json'
-        }
-        return got(edusignUrl, {headers}).json();
+        const payload: ISendDocumentBody = request.body
+        const fileBase64 = payload.file.toString('base64')
+        const studentId = await getStudentIdByEmail(payload.studentEmail)
+        const externalIds = await getExternalIdsByEmails(payload.externalEmails)
+
+        return {fileBase64, studentId, externalIds}
     })
 }
 
